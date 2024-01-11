0e27c5b9a656ad44c1651ff99535330b7ded7c03
Mirai-6568 <mitani.mirai@yellston.com>
2024-01-11 20:45:53 +0900

    add class

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ class Hello:
         print("a")
         print("b")
         print("c")
+
+class Hello2:
+    def __init__(self):
+        self.hello = "hello"
+    def __call__(self, *args, **kwargs):
+        print(self.hello)
+        print(self.new_hello())
+
+    def new_hello(self):
+        print("NEWHELLO")
+        print("HELLO")
+        print("HELLOOOO")
+        print("HELLOOOOOO")
+        print("a")
+        print("b")
+        print("c")
