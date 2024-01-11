f888e457cdfd1f5253a9dcea7c4486b35afd0464
Mirai-6568 <mitani.mirai@yellston.com>
2024-01-11 20:46:18 +0900

    remove function

--- main.py
+++ main.py
@@ -21,13 +21,4 @@ class Hello2:
         self.hello = "hello"
     def __call__(self, *args, **kwargs):
         print(self.hello)
-        print(self.new_hello())
 
-    def new_hello(self):
-        print("NEWHELLO")
-        print("HELLO")
-        print("HELLOOOO")
-        print("HELLOOOOOO")
-        print("a")
-        print("b")
-        print("c")
